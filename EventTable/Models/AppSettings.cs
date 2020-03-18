5297e0bc0a050d1b7ab8a68c2e1e742c60cf6d36
Daryayan <daryasa1991@mail.ru>
2020-03-18 19:44:19 +0300

    *fix configurations

--- EventTable/Models/AppSettings.cs
+++ EventTable/Models/AppSettings.cs
@@ -10,9 +10,9 @@ namespace EventTable.Models
     /// </summary>
     public static class AppSettings
     {
-        public static string Url { get; set; } = "https://telegrambotapp.azurewebsites.net:443/{0}";
+        public static string Url { get; set; } = "";
 
-        public static string Name { get; set; } = "extremecode_bot";
+        public static string Name { get; set; } = "EventTablebot";
 
         public static string Key { get; set; } = "1054347333:AAHsNZrggwnqFPV3vmgi1exYxMd5hMsuEH4";
 
