3ee6ad45c058247ade7d53b5aa1840317d710cc6
Daryayan <daryasa1991@mail.ru>
2020-05-18 00:26:45 +0300

    *Добавление файла конфигурации

--- EventTable/AppSettings.cs
+++ EventTable/AppSettings.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Configuration;
+using System.Collections.Specialized;
 
 namespace EventTable
 {
+
     public static class AppSettings
     {
         public static string Url { get; set; } = "https://git.heroku.com/eventtable.git";
 
         public static string Name { get; set; } = "EventTablebot";
 
-        public static string Key { get; set; } = "1054347333:AAHsNZrggwnqFPV3vmgi1exYxMd5hMsuEH4";
+        public static string Key { get; set; } = ConfigurationManager.ConnectionStrings["Token"].ConnectionString;
 
-        public static string DBConnection { get; } = "Database=event_table;Host=localhost;Username=postgres;Password=;Persist Security Info=True";
-
+        public static string DBConnection { get; } = ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
     }
 }
